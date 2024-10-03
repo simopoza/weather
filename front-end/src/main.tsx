6652cc03ed194cd18a3586afbcc0dd42ab5e0e17
simopoza <mannahri@student.1337.ma>
2024-10-03 13:27:28 +0100

    update main.tsx

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';  // Import ChakraProvider
 import App from './App.tsx';
-import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
